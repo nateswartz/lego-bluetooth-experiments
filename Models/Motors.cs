7a49da4f94331e0db91feee1654a157589ac25eb
nswartzentruber <nswartzentruber@gmail.com>
2021-01-21 05:59:01 -0500

    Update motor codes to match new firmware

--- Models/Motors.cs
+++ Models/Motors.cs
@@ -4,9 +4,9 @@ namespace SDKTemplate.Models
 {
     public class Motors
     {
-        public static Motor A = new Motor("A", "37");
-        public static Motor B = new Motor("B", "38");
-        public static Motor A_B = new Motor("A+B", "39");
+        public static Motor A = new Motor("A", "00");
+        public static Motor B = new Motor("B", "01");
+        public static Motor A_B = new Motor("A+B", "10");
         public static Motor External = new Motor("External", "00");
 
         public static List<Motor> All = new List<Motor>
