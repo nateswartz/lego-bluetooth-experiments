c7bdb02ab0d50d9b77bcf3b80e1ae7326852c611
nswartzentruber <nswartzentruber@gmail.com>
2021-01-22 16:07:27 -0500

    Move duplicated logic to parent class

---
+++ Commands/MotorCommand.cs
@@ -0,0 +1,25 @@
+﻿using SDKTemplate.Models;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SDKTemplate.Commands
+{
+    public abstract class MotorCommand
+    {
+        public async Task RunAsync(BoostController controller, string commandText, string clockwiseKeyword, Motor motor)
+        {
+            Match m = Regex.Match(commandText, @"\((\d+),(\d+)\)");
+            if (m.Groups.Count == 3)
+            {
+                var speed = Convert.ToInt32(m.Groups[1].Value);
+                var time = Convert.ToInt32(m.Groups[2].Value);
+                var clockWise = commandText.StartsWith(clockwiseKeyword);
+                await controller.RunMotor(motor, speed, time, clockWise);
+                await Task.Delay(time);
+            }
+        }
+    }
+}
+
+
--- Commands/RoverMoveCommand.cs
+++ Commands/RoverMoveCommand.cs
@@ -1,26 +1,16 @@
 ﻿using SDKTemplate.Models;
-using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SDKTemplate.Commands
 {
-    public class RoverMoveCommand : ICommand
+    public class RoverMoveCommand : MotorCommand, ICommand
     {
         public IEnumerable<string> Keywords { get => new List<string> { "forward", "back" }; }
 
         public async Task RunAsync(BoostController controller, string commandText)
         {
-            Match m = Regex.Match(commandText, @"\((\d+),(\d+)\)");
-            if (m.Groups.Count == 3)
-            {
-                var speed = Convert.ToInt32(m.Groups[1].Value);
-                var time = Convert.ToInt32(m.Groups[2].Value);
-                var forward = commandText.StartsWith("forward");
-                await controller.RunMotor(Motors.A_B, speed, time, forward);
-                await Task.Delay(time);
-            }
+            await RunAsync(controller, commandText, "forward", Motors.A_B);
         }
     }
 }
