c4d92328df5605978d420843a16fb3e91cb1dd6b
nswartzentruber <nswartzentruber@gmail.com>
2021-01-27 14:24:46 -0500

    Improve Voltage notification

--- Responses/VoltageData.cs
+++ Responses/VoltageData.cs
@@ -1,15 +1,19 @@
-﻿namespace LegoBoostController.Responses
+﻿using System;
+
+namespace LegoBoostController.Responses
 {
     public class VoltageData : SensorData
     {
+        public int Voltage { get; set; }
+
         public VoltageData(string body) : base(body)
         {
-            NotificationType = GetType().Name;
+            Voltage = Convert.ToInt32(body.Substring(8, 4), 16);
         }
 
         public override string ToString()
         {
-            return $"Voltage Sensor Data ({Port}) : {Body}";
+            return $"Voltage Sensor Data ({Port}) : {Voltage} - {Body}";
         }
     }
 }
