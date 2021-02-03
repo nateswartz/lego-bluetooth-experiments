78405fa97023ceac2465115914b64b96ce2601e1
nswartzentruber <nswartzentruber@gmail.com>
2021-02-03 05:20:42 -0500

    Re-organize project structure and remove unused projects

--- BluetoothBase/Bluetooth/BluetoothDeviceInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BluetoothBase.Bluetooth
-{
-    public sealed class BluetoothDeviceInfo
-    {
-        public ulong BluetoothAddress { get; set; }
-        public byte[] ManufacturerData { get; set; }
-        public string Name { get; set; }
-    }
-}
