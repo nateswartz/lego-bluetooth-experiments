944a749b283e3ce706c5ab4ff0bf9446820bccf8
nswartzentruber <nswartzentruber@gmail.com>
2021-02-08 15:40:37 -0500

    Add more detail to the error response

--- BluetoothController/Responses/Error.cs
+++ BluetoothController/Responses/Error.cs
@@ -2,13 +2,29 @@
 {
     public class Error : Response
     {
+        public string ErrorType { get; set; }
+
         public Error(string body) : base(body)
         {
+            var errorType = Body.Substring(8, 2);
+            switch (errorType)
+            {
+                case "05":
+                    ErrorType = "Command Not Recognized";
+                    break;
+                case "06":
+                    ErrorType = "Invalid Use";
+                    break;
+                default:
+                    ErrorType = "Unknown";
+                    break;
+            }
+            NotificationType = GetType().Name;
         }
 
         public override string ToString()
         {
-            return $"Error: Received Unrecognized Command (Response Body: {Body}";
+            return $"Error: {ErrorType} (Response Body: {Body})";
         }
     }
 }
