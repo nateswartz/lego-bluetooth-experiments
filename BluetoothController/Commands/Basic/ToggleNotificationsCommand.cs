4c21ded4dc677eb0e3e6a4c19c1374008693065a
nswartzentruber <nswartzentruber@gmail.com>
2021-02-15 06:06:19 -0500

    Add comment for external motor notifications

--- BluetoothController/Commands/Basic/ToggleNotificationsCommand.cs
+++ BluetoothController/Commands/Basic/ToggleNotificationsCommand.cs
@@ -9,6 +9,7 @@ namespace BluetoothController.Commands.Basic
         // TODO: Make sensor mode more user friendly
         // For Tilt Sensor, 01-04 all work, vary granularity
         // For Internal Motor (single/combined), 01-02 work, 03-04 don't
+        // For External Motor, 02 is angle data, 01 is speed data, 03 is more granular something, 04 doesn't work
         public ToggleNotificationsCommand(string portId, bool enableNotifications, string sensorMode)
         {
             var state = enableNotifications ? "01" : "00"; // 01 - On; 00 - Off
