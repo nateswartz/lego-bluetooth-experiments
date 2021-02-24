324065031830d0b68a1da85ec52d8492e7366df7
nswartzentruber <nswartzentruber@gmail.com>
2021-02-24 05:47:03 -0500

    Begin filling out PortInfo response

--- BluetoothController/Responses/Device/Info/PortInfo.cs
+++ BluetoothController/Responses/Device/Info/PortInfo.cs
@@ -1,19 +1,30 @@
-﻿namespace BluetoothController.Responses.Device.Info
+﻿using System;
+
+namespace BluetoothController.Responses.Device.Info
 {
+    public enum InformationType
+    {
+        ModeInfo = 1,
+        PossibleModeCombinations = 2
+    }
+
     public class PortInfo : Response
     {
         public string Port { get; set; }
+        public InformationType InfoType { get; set; }
+
 
         public PortInfo(string body) : base(body)
         {
             Port = body.Substring(6, 2);
+            InfoType = (InformationType)Convert.ToInt32(body.Substring(8, 2), 16);
 
             NotificationType = GetType().Name;
         }
 
         public override string ToString()
         {
-            return $"Port Info ({Port}) [{Body}]";
+            return $"Port Info ({Port}) {Enum.GetName(typeof(InformationType), InfoType)} [{Body}]";
         }
     }
 }
