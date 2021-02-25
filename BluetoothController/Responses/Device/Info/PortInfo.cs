d45eebe33f016bcad1b3309f09c701e08f8ceffe
nswartzentruber <nswartzentruber@gmail.com>
2021-02-25 08:35:18 -0500

    Continue refining PortInfo

--- BluetoothController/Responses/Device/Info/PortInfo.cs
+++ BluetoothController/Responses/Device/Info/PortInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BluetoothController.Responses.Device.Info
 {
@@ -8,23 +9,40 @@ namespace BluetoothController.Responses.Device.Info
         PossibleModeCombinations = 2
     }
 
+    public enum Capability
+    {
+        Output = 0,
+        Input = 1,
+        LogicalCombinable = 2,
+        LogicalSynchronizable = 4
+    }
+
     public class PortInfo : Response
     {
         public string Port { get; set; }
         public InformationType InfoType { get; set; }
-
+        public List<Capability> Capabilities { get; set; }
 
         public PortInfo(string body) : base(body)
         {
+            Capabilities = new List<Capability>();
             Port = body.Substring(6, 2);
             InfoType = (InformationType)Convert.ToInt32(body.Substring(8, 2), 16);
-
+            var capabilityBitField = (Capability)Convert.ToInt32(body.Substring(10, 2));
+            if ((capabilityBitField & Capability.Output) == Capability.Output)
+                Capabilities.Add(Capability.Output);
+            if ((capabilityBitField & Capability.Input) == Capability.Input)
+                Capabilities.Add(Capability.Input);
+            if ((capabilityBitField & Capability.LogicalCombinable) == Capability.LogicalCombinable)
+                Capabilities.Add(Capability.LogicalCombinable);
+            if ((capabilityBitField & Capability.LogicalSynchronizable) == Capability.LogicalSynchronizable)
+                Capabilities.Add(Capability.LogicalSynchronizable);
             NotificationType = GetType().Name;
         }
 
         public override string ToString()
         {
-            return $"Port Info ({Port}) {Enum.GetName(typeof(InformationType), InfoType)} [{Body}]";
+            return $"Port Info ({Port}) {Enum.GetName(typeof(InformationType), InfoType)}; Capability Count: {Capabilities.Count} [{Body}]";
         }
     }
 }
