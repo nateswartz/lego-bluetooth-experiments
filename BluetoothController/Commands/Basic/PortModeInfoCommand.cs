b776c166fbdd805ab387efda544700dd4f83a74f
nswartzentruber <nswartzentruber@gmail.com>
2021-03-02 05:32:42 -0500

    Move enum into separate file

--- BluetoothController/Commands/Basic/PortModeInfoCommand.cs
+++ BluetoothController/Commands/Basic/PortModeInfoCommand.cs
@@ -3,20 +3,6 @@ using BluetoothController.Models;
 
 namespace BluetoothController.Commands.Basic
 {
-    public enum ModeInfoType
-    {
-        Name = 0,
-        Raw = 1,
-        Percent = 2,
-        Si = 3,
-        Symbol = 4,
-        Mapping = 5,
-        Internal = 6,
-        MotorBias = 7,
-        CapabilityBits = 8,
-        ValueFormat = 128
-    }
-
     public class PortInfoModeCommand : CommandType
     {
         public PortInfoModeCommand(string portId, string mode, ModeInfoType type) : base(MessageTypes.PortModeInformationRequest)
---
+++ BluetoothController/Models/ModeInfoType.cs
@@ -0,0 +1,18 @@
+﻿namespace BluetoothController.Models
+{
+    public enum ModeInfoType
+    {
+        Name = 0,
+        Raw = 1,
+        Percent = 2,
+        Si = 3,
+        Symbol = 4,
+        Mapping = 5,
+        Internal = 6,
+        MotorBias = 7,
+        CapabilityBits = 8,
+        ValueFormat = 128
+    }
+}
+
+
