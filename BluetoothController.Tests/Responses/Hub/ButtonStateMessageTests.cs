63bdd2930335c06cec8ca00a727e55e3e3f70571
nswartzentruber <nswartzentruber@gmail.com>
2021-03-15 20:27:53 -0400

    Add tests for ButtonStateMessage

---
+++ BluetoothController.Tests/Responses/Hub/ButtonStateMessageTests.cs
@@ -0,0 +1,20 @@
+﻿using BluetoothController.Responses.Hub;
+using Xunit;
+
+namespace BluetoothController.Tests.EventHandlers
+{
+    public class ButtonStateMessageTests
+    {
+        [Theory]
+        [InlineData("00", ButtonState.Released)]
+        [InlineData("01", ButtonState.Pressed)]
+        [InlineData("06", ButtonState.Unknown)]
+        public void Constructor_SetsCorrectValue(string buttonStateByte, ButtonState expectedState)
+        {
+            var irrelevantBytes = "0000000000";
+            var buttonStateMessage = new ButtonStateMessage($"{irrelevantBytes}{buttonStateByte}");
+
+            Assert.Equal(expectedState, buttonStateMessage.State);
+        }
+    }
+}
---
+++ BluetoothController/Responses/Hub/ButtonStateMessage.cs
@@ -0,0 +1,32 @@
+﻿namespace BluetoothController.Responses.Hub
+{
+    public enum ButtonState
+    {
+        Released,
+        Pressed,
+        Unknown,
+    }
+
+    public class ButtonStateMessage : HubInfo
+    {
+        public ButtonState State { get; set; }
+
+        public ButtonStateMessage(string body) : base(body)
+        {
+            switch (Body.Substring(10, 2))
+            {
+                case "00":
+                    State = ButtonState.Released;
+                    break;
+                case "01":
+                    State = ButtonState.Pressed;
+                    break;
+                default:
+                    State = ButtonState.Unknown;
+                    break;
+            }
+        }
+
+        public override string ToString() => $"Button State: {State} [{Body}]";
+    }
+}
