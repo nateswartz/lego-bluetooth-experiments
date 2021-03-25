d0f39f30f93d14a2433bd52177b123e8a6b6723d
nswartzentruber <nswartzentruber@gmail.com>
2021-03-25 06:01:58 -0400

    Break up method into smaller methods

--- BluetoothController/Responses/Device/Info/PortInfo.cs
+++ BluetoothController/Responses/Device/Info/PortInfo.cs
@@ -10,11 +10,11 @@ namespace BluetoothController.Responses.Device.Info
     {
         public string Port { get; set; }
         public InformationType InfoType { get; set; }
-        public List<Capability> Capabilities { get; set; } = new List<Capability>();
+        public IEnumerable<Capability> Capabilities { get; set; }
         public int TotalModeCount { get; set; }
-        public List<int> InputModes { get; set; } = new List<int>();
-        public List<int> OutputModes { get; set; } = new List<int>();
-        public List<string> ModeCombinations { get; set; } = new List<string>();
+        public IEnumerable<int> InputModes { get; set; }
+        public IEnumerable<int> OutputModes { get; set; }
+        public IEnumerable<string> ModeCombinations { get; set; }
 
         public PortInfo(string body) : base(body)
         {
@@ -22,47 +22,68 @@ namespace BluetoothController.Responses.Device.Info
             InfoType = (InformationType)Convert.ToInt32(body.Substring(8, 2), 16);
             if (InfoType == InformationType.ModeInfo)
             {
-                var capabilityBitField = (Capability)Convert.ToInt32(body.Substring(10, 2), 16);
-                foreach (var value in Enum.GetValues(typeof(Capability)))
-                {
-                    var capability = (Capability)Enum.Parse(typeof(Capability), value.ToString());
-                    if ((capabilityBitField & capability) == capability)
-                        Capabilities.Add(capability);
-                }
-                TotalModeCount = Convert.ToInt32(body.Substring(12, 2), 16);
-                var inputModesBitField = Convert.ToInt32(body.Substring(14, 4), 16);
-                for (var bit = 1; bit <= Math.Pow(2.0, 15.0); bit *= 2)
-                {
-                    if ((inputModesBitField & bit) == bit)
-                        InputModes.Add(Convert.ToInt32(Math.Log(Convert.ToDouble(bit), 2.0)));
-                }
-                var outputModesBitField = Convert.ToInt32(body.Substring(18, 4), 16);
-                for (var bit = 1; bit <= Math.Pow(2.0, 15.0); bit *= 2)
-                {
-                    if ((outputModesBitField & bit) == bit)
-                        OutputModes.Add(Convert.ToInt32(Math.Log(Convert.ToDouble(bit), 2.0)));
-                }
+                ExtractModeInfo(body);
             }
             else if (InfoType == InformationType.PossibleModeCombinations)
             {
-                if (body.Length > 10)
+                ModeCombinations = ExtractPossibleModeCombinations(body);
+            }
+        }
+
+        private void ExtractModeInfo(string body)
+        {
+            Capabilities = ExtractCapabilities(body.Substring(10, 2));
+            TotalModeCount = Convert.ToInt32(body.Substring(12, 2), 16);
+            InputModes = ExtractModes(body.Substring(14, 4));
+            OutputModes = ExtractModes(body.Substring(18, 4));
+        }
+
+        private IEnumerable<Capability> ExtractCapabilities(string capabilitySection)
+        {
+            var capabilities = new List<Capability>();
+            var capabilityBitField = (Capability)Convert.ToInt32(capabilitySection, 16);
+            foreach (var value in Enum.GetValues(typeof(Capability)))
+            {
+                var capability = (Capability)Enum.Parse(typeof(Capability), value.ToString());
+                if ((capabilityBitField & capability) == capability)
+                    capabilities.Add(capability);
+            }
+            return capabilities;
+        }
+
+        private IEnumerable<int> ExtractModes(string modesSection)
+        {
+            var modes = new List<int>();
+            var inputModesBitField = Convert.ToInt32(modesSection, 16);
+            for (var bit = 1; bit <= Math.Pow(2.0, 15.0); bit *= 2)
+            {
+                if ((inputModesBitField & bit) == bit)
+                    modes.Add(Convert.ToInt32(Math.Log(Convert.ToDouble(bit), 2.0)));
+            }
+            return modes;
+        }
+
+        private IEnumerable<string> ExtractPossibleModeCombinations(string body)
+        {
+            var modeCombinations = new List<string>();
+            if (body.Length > 10)
+            {
+                var index = 10;
+                var combination = Convert.ToInt32(body.Substring(index, 2), 16);
+                while (combination != 0)
                 {
-                    var index = 10;
-                    var combination = Convert.ToInt32(body.Substring(index, 2), 16);
-                    while (combination != 0)
+                    var readableCombo = "[";
+                    for (var bit = 1; bit <= Math.Pow(2.0, 15.0); bit *= 2)
                     {
-                        var readableCombo = "[";
-                        for (var bit = 1; bit <= Math.Pow(2.0, 15.0); bit *= 2)
-                        {
-                            if ((combination & bit) == bit)
-                                readableCombo += $"{Convert.ToInt32(Math.Log(Convert.ToDouble(bit), 2.0))} ";
-                        }
-                        ModeCombinations.Add($"{readableCombo.Trim()}]");
-                        index += 2;
-                        combination = Convert.ToInt32(body.Substring(index, 2), 16);
+                        if ((combination & bit) == bit)
+                            readableCombo += $"{Convert.ToInt32(Math.Log(Convert.ToDouble(bit), 2.0))} ";
                     }
+                    modeCombinations.Add($"{readableCombo.Trim()}]");
+                    index += 2;
+                    combination = Convert.ToInt32(body.Substring(index, 2), 16);
                 }
             }
+            return modeCombinations;
         }
 
         public override string ToString()
