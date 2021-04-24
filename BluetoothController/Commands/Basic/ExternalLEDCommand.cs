a080f3dc8e21d4212c4eb01919df97158c2b10e0
Nate Swartzentruber <nswartzentruber@gmail.com>
2021-04-23 21:13:33 -0400

    Add command to set External LED brightness

---
+++ BluetoothController/Commands/Basic/ExternalLEDCommand.cs
@@ -0,0 +1,16 @@
+﻿using BluetoothController.Commands.Abstract;
+using BluetoothController.Controllers;
+using BluetoothController.Models;
+using System.Linq;
+
+namespace BluetoothController.Commands.Basic
+{
+    public class ExternalLEDCommand : PortOutputCommandType
+    {
+        public ExternalLEDCommand(IHubController controller, string power)
+        {
+            var port = controller.Hub.GetPortsByDeviceType(IOTypes.ExternalLED).First().PortID;
+            HexCommand = AddHeader($"{port}115100{power}");
+        }
+    }
+}
