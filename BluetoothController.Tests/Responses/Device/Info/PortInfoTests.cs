e16e3046f81ae4601a5b376b43b7e74e45e4dcbb
Nate Swartzentruber <nswartzentruber@gmail.com>
2021-04-28 05:59:13 -0400

    Additional tests

---
+++ BluetoothController.Tests/Responses/Device/Info/PortInfoTests.cs
@@ -0,0 +1,31 @@
+﻿using BluetoothController.Models.Enums;
+using BluetoothController.Responses.Device.Info;
+using Xunit;
+
+namespace BluetoothController.Tests.Responses.Device.Info
+{
+    public class PortInfoTests
+    {
+        [Fact]
+        public void Constructor_SetsCorrectPort()
+        {
+            var header = "000000";
+            var footer = "000000000000000";
+            var messageBody = $"{header}02{footer}";
+            var portInfoMessage = new PortInfo(messageBody);
+
+            Assert.Equal("02", portInfoMessage.Port);
+        }
+
+        [Fact]
+        public void Constructor_SetsCorrectInformationType()
+        {
+            var header = "00000000";
+            var footer = "0000000000000";
+            var messageBody = $"{header}01{footer}";
+            var portInfoMessage = new PortInfo(messageBody);
+
+            Assert.Equal(InformationType.ModeInfo, portInfoMessage.InfoType);
+        }
+    }
+}
